0e418e1781a6e5fe80a18b1f28697afda37b00d9
DesDheressa <33158446+DesDheressa@users.noreply.github.com>
2017-12-15 13:04:21 -0500

    test1

--- disk_space.ps1
+++ disk_space.ps1
@@ -9,3 +9,5 @@ Select PSComputername,DeviceID,
 @{Name="SizeGB";Expression={$_.Size/1GB -as [int]}},
 @{Name="FreeGB";Expression={[math]::Round($_.Freespace/1GB,2)}} | Out-File C:\Users\desdheressa\Documents\GitLab\Database\AdHocScripts\disk_space.txt
 Get-Date | out-file C:\Users\desdheressa\Documents\GitLab\Database\AdHocScripts\disk_space.txt -append;
+
+#test change
