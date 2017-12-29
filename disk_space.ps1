5328817bc87dd373b45f422b0ea8be6ad443a5b1
DesDheressa <33158446+DesDheressa@users.noreply.github.com>
2017-12-29 16:47:18 -0500

    directory changed

--- disk_space.ps1
+++ disk_space.ps1
@@ -7,7 +7,7 @@ Select PSComputername,DeviceID,Size,Freespace
 Get-WMIObject Win32_Logicaldisk -filter "deviceid='C:'" -ComputerName MS000004767 |
 Select PSComputername,DeviceID,
 @{Name="SizeGB";Expression={$_.Size/1GB -as [int]}},
-@{Name="FreeGB";Expression={[math]::Round($_.Freespace/1GB,2)}} | Out-File C:\Users\desdheressa\Documents\GitLab\Database\AdHocScripts\disk_space.txt
-Get-Date | out-file C:\Users\desdheressa\Documents\GitLab\Database\AdHocScripts\disk_space.txt -append;
+@{Name="FreeGB";Expression={[math]::Round($_.Freespace/1GB,2)}} | Out-File C:\Users\desdheressa\Documents\GitLab\Database\hello-world\disk_space.txt
+Get-Date | out-file C:\Users\desdheressa\Documents\GitLab\Database\hello-world\disk_space.txt -append;
 
 #test1
